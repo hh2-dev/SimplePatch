70a7d55240ce9e65280af6e6864f96a68538b0cf
HH2\jchristensen <jchristensen@hh2.com>
2023-10-05 17:06:06 -0600

    Modify solution to sign assemblies with our snk file.

--- src/SimplePatch/Properties/AssemblyInfo.cs
+++ src/SimplePatch/Properties/AssemblyInfo.cs
@@ -1,6 +1,11 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("SimplePatch.Tests")]
+[assembly: InternalsVisibleTo("SimplePatch.Tests,PublicKey="+
+"00240000048000009400000006020000002400005253413100040000010001009b9fbdd94ee991"+
+"33c6d38c46894e87490b24ecdd3705b34388d8866be3bc5f0f9213adee7406fa6dab8479df06dc"+
+"d12b7e6e5b72aaa559fd4f3dd3023922a3b9ba5e5022b1d2518a386b3fbf4d3546e872a92b6ab2"+
+"21c75ccdf8dfc07b5f3a0c42df31e1a6f44910f2c30c3d2cf38652a5e80f040f5307ea448f87fc"+
+"81dae5b4")]
 namespace SimplePatch
 {
     class AssemblyInfo
